90c7ac6017c23f426d8315cddf3feff676b4c7e1
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-01-13 22:24:16 +0530

    AS

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -13,7 +13,7 @@ const CustomNavLink = ({ to,title, children }) => (
        `}>
 
        <div className="flex flex-col">
-       <span>{title}</span>
+       <span className="eff-hover">{title}</span>
        <span className="eff"></span>
 
        </div>
@@ -64,10 +64,11 @@ export default function Header() {
           <div className="flex items-center lg:order-2">
             <Link
               to="#"
-              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className="bg-gray-50 text-black font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Log in
             </Link>
+
             <Link
               to="#"
               className="sp-btn text-white focus:ring-4 font-semibold rounded-lg text-sm px-4  lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
