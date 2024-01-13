b9ae9e08b14140e8819c08530d47adf7b998fc8b
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-01-13 14:21:08 +0530

    imp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -11,7 +12,7 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
       <Route path="user/:userid" element={<User />} />
-      
+
     </Route>
   )
 );
