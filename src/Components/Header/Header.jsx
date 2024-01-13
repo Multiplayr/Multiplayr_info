0d8103a875c24a6dbd6cc9c2fb37e795268a52e1
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-01-13 21:56:10 +0530

    aa

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,16 +3,21 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 
-const CustomNavLink = ({ to, children }) => (
+const CustomNavLink = ({ to,title, children }) => (
   <NavLink
     to={to}
     className={({ isActive }) => `
-                 block py-2 pr-4 pl-3 duration-200 font-heading
-                ${isActive ? "text-white" : "text-gray-700"}
-                border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent
-               lg:border-0 hover:text-primary lg:p-0
-       `}
-  >
+                 flex py-2 pr-4 pl-3 duration-200 font-heading
+                ${isActive ? "text-white font-bold " : "text-gray-400"}
+               lg:border-0 lg:p-0
+       `}>
+
+       <div className="flex flex-col">
+       <span>{title}</span>
+       <span className="eff"></span>
+
+       </div>
+
     {children}
   </NavLink>
 );
@@ -20,7 +25,7 @@ const CustomNavLink = ({ to, children }) => (
 export default function Header() {
   return (
     <header className="shadow sticky z-50 top-0">
-      <nav className="bg-bg border-gray-200 px-4 lg:px-6 py-2.5">
+      <nav className="bg-black border-gray-200 px-4 lg:px-6 py-5 ">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img
@@ -36,18 +41,22 @@ export default function Header() {
           >
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
-                <CustomNavLink to="/">
-                  Home
-                </CustomNavLink>
+                <CustomNavLink to="/" title="Home"></CustomNavLink>
               </li>
               <li>
-                <CustomNavLink to="/about">About</CustomNavLink>
+                <CustomNavLink to="/features" title="Features"></CustomNavLink>
               </li>
               <li>
-                <CustomNavLink to="/contact">Contact</CustomNavLink>
+                <CustomNavLink to="/working" title="How It Works"></CustomNavLink>
               </li>
               <li>
-                <CustomNavLink to="/github">Github</CustomNavLink>
+                <CustomNavLink to="/whatyouget" title="What You Get"></CustomNavLink>
+              </li>
+              <li>
+                <CustomNavLink to="/about" title="About Us"></CustomNavLink>
+              </li>
+              <li>
+                <CustomNavLink to="/projects" title="Projects"></CustomNavLink>
               </li>
             </ul>
           </div>
@@ -61,7 +70,7 @@ export default function Header() {
             </Link>
             <Link
               to="#"
-              className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              className="sp-btn text-white focus:ring-4 font-semibold rounded-lg text-sm px-4  lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Get started
             </Link>
