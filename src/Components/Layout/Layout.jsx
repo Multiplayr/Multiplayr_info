02c25345a9bbfcbf96553bbc8340b8772a15b5c6
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-01-14 13:05:30 +0530

    aa

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -4,10 +4,6 @@ import Feature from '../Feature/Feature'
 import { Outlet } from 'react-router-dom';
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0c1c536f5ee056e0309051e9483f3a388077bd43
 function Layout() {
   return (
     <>
