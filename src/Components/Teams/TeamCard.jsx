071eb5916eec25ab31838c364ca099dcbb1b7cde
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-01-14 20:32:54 +0530

    swayam

--- src/Components/Teams/TeamCard.jsx
+++ src/Components/Teams/TeamCard.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import styles from "./TeamCard.module.css"
-import 'font-awesome/css/font-awesome.min.css';
+import React from "react";
+import styles from "./TeamCard.module.css";
+import "font-awesome/css/font-awesome.min.css";
 import "./TeamCard.module.css";
-
+import { Link } from "react-router-dom";
 
 export const TeamCard = ({ member: { image, name, position } }) => {
   return (
-    <div className={styles.container }>
-
+    <div className={styles.container}>
       <div className={styles.image}>
-      <img src={image} alt='projecttitle'/>
+        <img src={image} alt="projecttitle" />
       </div>
-      
+
       <h3 className={styles.title}>{name}</h3>
       <p className={styles.description}>{position}</p>
 
-
       <div className={styles.social}>
-      <i className="fa fa-facebook" aria-hidden="true"></i>
-      <i className="fa fa-instagram" aria-hidden="true"></i>
-      <i className="fa fa-twitter" aria-hidden="true"></i>
-      <i className="fa fa-linkedin" aria-hidden="true"></i>
+        <Link to={"#"}>
+          <i className="fa fa-facebook" aria-hidden="true"></i>{" "}
+        </Link>
+        <Link to={"#"}>
+          <i className="fa fa-instagram" aria-hidden="true"></i>
+        </Link>
+        <Link to={"#"}>
+          <i className="fa fa-twitter" aria-hidden="true"></i>
+        </Link>
+        <Link to={"#"}>
+          <i className="fa fa-linkedin" aria-hidden="true"></i>
+        </Link>
       </div>
+      
       {/* <ul className={styles.skills}>
         {
           skills.map((skill, id) => {
@@ -39,5 +46,5 @@ export const TeamCard = ({ member: { image, name, position } }) => {
         <a href={source} target='_blank' className={styles.link}>Source</a>
       </div> */}
     </div>
-  )
-}
+  );
+};
