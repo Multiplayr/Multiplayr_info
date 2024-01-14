4e636f062859834f707e27fe18834f7a4b2e5a37
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-01-14 19:31:15 +0530

    layout fixes

---
+++ src/Components/Working/ImageContainer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function ImageContainer({src,title,description}) {
+  return (
+    <>
+        <div className="imageContainer">
+          <img
+            src={src}
+            alt="Working-img"
+            className="image"
+          />
+
+          <div className="image_description">
+            <div className="image_title">
+              <span>{title}</span>
+            </div>
+
+            <div className="image_about">
+              <p>
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default ImageContainer
--- src/Components/Working/Working.jsx
+++ src/Components/Working/Working.jsx
@@ -1,47 +1,88 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import "./Working.css"
+import "./Working.css";
+import ImageContainer from "./ImageContainer";
 
 const Working = () => {
-    return (
-        <div className='container' id="pico">
-            <div className='head'>
-                <h3 className='head_about'>how it works</h3>
-                <h1 className='head_title'>The Ignition Of Metaverse</h1>
-            </div>
-            <ul className='listItems'>
-                <li className='listItem'>Metaverse Marketplace</li>
-                <li className='listItem'>Launchpad</li>
-                <li className='listItem'>MetaFi DeFi Valut</li>
+  const [selectedBtn, setSelectedBtn] = useState(0);
 
+  const a = [
+    {
+      title: " Metaverse Marketplace",
+      src: "assets/images/works/workImage1.jpg",
 
-            </ul>
-            <div className='imageContainer'>
-                <img src='assets/images/works/workImage.jpg' alt='Working-img' className='image' />
+      des: `A Gamified Marketplace Dedicated To The Metaverse And Gaming
+    Assets. The Metaverse Marketplace Allows Users To Trade,
+    Auction, And Rent Virtual Ownership Assets From Various
+    Metaverse And Gaming Projects. The Metaverse Is The Next
+    Generation Of The Internet.`,
+    },
 
-                <div className='image_description'>
-                <div className='image_title'>
-                    <h2>Metaverse Marketplace</h2>
+    {
+      title: " Launchpad",
+      src: "assets/images/works/workImage2.jpg",
 
-                </div>
-                
+      des: `A Gamified Marketplace Dedicated To The Metaverse And Gaming
+    Assets. The Metaverse Marketplace Allows Users To Trade,
+    Auction, And Rent Virtual Ownership Assets From Various
+    Metaverse And Gaming Projects. The Metaverse Is The Next
+    Generation Of The Internet.`,
+    },
 
+    {
+      title: " MetaFi DeFi Vault",
+      src: "assets/images/works/workImage3.jpg",
 
-                <div className='image_about'>
-                    <p>A Gamified Marketplace Dedicated To The Metaverse And Gaming Assets.
-                        The Metaverse Marketplace Allows Users To Trade, Auction, And Rent Virtual Ownership Assets From Various Metaverse And Gaming Projects.
-                        The Metaverse Is The Next Generation Of The Internet.</p>
+      des: `A Gamified Marketplace Dedicated To The Metaverse And Gaming
+    Assets. The Metaverse Marketplace Allows Users To Trade,
+    Auction, And Rent Virtual Ownership Assets From Various
+    Metaverse And Gaming Projects. The Metaverse Is The Next
+    Generation Of The Internet.`,
+    },
+  ];
 
-                </div>
-                </div>
-            </div>
+  return (
+    <div className="working-layout">
+      <div className="container" id="pico">
+        <img
+          className="overlay-rot-img-b"
+          src="/assets/images/banner/banner-shape-4.png"
+          alt=""
+        />
+
+        <div className="head">
+          <h3 className="head_about">how it works</h3>
+          <h1 className="head_title">The Ignition Of Metaverse</h1>
         </div>
 
-
-
-
-
-    )
-}
+        <ul className="listItems">
+          <li
+            className={`listItem ${selectedBtn == 0 ? "select-focus" : ""}`}
+            onClick={() => setSelectedBtn(0)}
+          >
+            Metaverse Marketplace
+          </li>
+          <li
+            className={`listItem ${selectedBtn == 1 ? "select-focus" : ""}`}
+            onClick={() => setSelectedBtn(1)}
+          >
+            Launchpad
+          </li>
+          <li
+            className={`listItem ${selectedBtn == 2 ? "select-focus" : ""}`}
+            onClick={() => setSelectedBtn(2)}
+          >
+            MetaFi DeFi Valut
+          </li>
+        </ul>
+        <ImageContainer
+          src={a[selectedBtn].src}
+          title={a[selectedBtn].title}
+          description={a[selectedBtn].des}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Working;
