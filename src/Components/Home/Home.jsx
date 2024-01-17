62cbe15c97cec01f0437ba10680b750eff204178
Swayam Kumar Karn <rohankukarn75@gmail.com>
2024-01-17 17:40:43 +0530

    home.css update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -45,7 +45,7 @@ function Home() {
 
               <Link
                   to={"https://litepaper.multiplayr.gg/quick-introduction/legal-disclaimer"}
-                className="sp-btn2 text-black font-heading focus:ring-4 font-semibold rounded-lg text-base px-7 lg:px-10 py-2 lg:py-2.5 mr-2 focus:outline-none z-50"
+                className="sp-btn2 text-black font-heading focus:ring-4 font-semibold rounded-lg text-base px-7 lg:px-7 py-2 lg:py-2 mr-2 focus:outline-none z-50"
               >
                 Whitepaper
               </Link>
